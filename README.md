511de234c1ad2459aec1393792c8a7edf500f1c7
Brent McPhail <bmcphail@thinkbrave.com>
2026-04-06 16:40:50 -0400

    copy files and add imu_data_publisher. make all pubs optional

--- README.md
+++ README.md
@@ -1,1 +1,177 @@
 # Inertial Labs Driver
+
+This packaged was copied from https://us.inertiallabs.com:31443/scm/ins/inertiallabs-ros2-pkgs.git
+on April 1st, 2026. On April 6th, 2026, the website was no longer accessible. 
+
+Changes:
+- Made all publishers optional thru params
+  - parameter options:
+    - publish_imu_data <- this is what we want
+    - publish_sensor_data
+    - publish_ins_data
+    - publish_gps_data  
+    - publish_gnss_data
+    - publish_marine_data
+- Added a new publisher that puts the 9 DOF imu data in the same message (was split across two separate subscriptions)
+  - If this wasn't done, the consumer would have to receive to separate messages and align timestamps to combine them
+
+
+---
+
+Below is the original README for the package:
+
+---
+
+# inertiallabs_ros2_pkgs
+Linux ROS driver for [Inertial Labs](https://inertiallabs.com/) products.
+Supported devices: INS, IMU-P, AHRS, AHRS-10.
+
+The package is developed based on the official `SDK v0.2` for Linux.
+
+The package is tested on:
+- [ROS2 Humble Hawksbill](https://docs.ros.org/en/humble/Installation.html) with Ubuntu 22.04 LTS
+- [ROS2 Iron Irwini](https://docs.ros.org/en/iron/Installation/Ubuntu-Install-Debs.html) with Ubuntu 22.04 LTS
+
+## License
+* The license for the official SDK is the MIT license.
+* The license for the other codes is Apache 2.0 whenever not specified.
+
+## Build
+It builds by colcon build-system.
+Make sure that your folder with ROS2 packages included in environment variable `ROS_PACKAGE_PATH`.
+Clone your package in `<your_work_space>/src`.
+And build with the Colcon.
+
+```bash
+cd <your_work_space>/src
+git clone https://us.inertiallabs.com:31443/scm/ins/inertiallabs-ros2-pkgs.git
+cd <your_work_space>
+colcon build
+```
+
+## Run
+```bash
+sudo chmod 666 /dev/ttyUSB0
+sudo stty -F /dev/ttyUSB0 115200
+source install/setup.bash
+ros2 run inertiallabs_ins il_ins --ros-args -p ins_url:=serial:/dev/ttyUSB0:115200 -p ins_output_format:=51
+```
+
+For ins OPVT2AHR packet via USB serial port:
+```bash
+ros2 run inertiallabs_ins il_ins --ros-args -p ins_url:=serial:/dev/ttyUSB0:115200 -p ins_output_format:=88
+```
+For ins OPVT packet via UDP (INS hostname is used):
+```bash
+ros2 run inertiallabs_ins il_ins --ros-args -p ins_url:=udp:INS-F2001234:23 -p ins_output_format:=82
+```
+For ins OPVT packet via UDP (INS IP address is used):
+```bash
+ros2 run inertiallabs_ins il_ins --ros-args -p ins_url:=udp:192.168.0.249:23 -p ins_output_format:=82
+```
+
+**Parameters**
+
+`ins_url` (`string`, `default: serial:/dev/ttyUSB0:460800`)
+Port the device is connected to. Can be:
+- serial:[path to device]:[baudrate]
+- tcp:[hostname or address]:[tcp server port]
+- udp:[hostname or address]:[udp server port]
+
+Inertial Labs Driver supports serial connection.
+
+`ins_output_format` (`int`, `default: 82`)
+The output data format of the INS data according to IL INS ICD.
+```
+ IL_IMU_Orientation         51  (0x33)
+ IL_SENSOR_DATA             80  (0x50)
+ IL_OPVT                    82  (0x52)
+ IL_MINIMAL_DATA            83  (0x53)
+ IL_QPVT                    86  (0x56)
+ IL_OPVT2A                  87  (0x57)
+ IL_OPVT2AHR                88  (0x58)
+ IL_OPVT2AW                 89  (0x59)
+ IL_OPVTAD                  97  (0x61)
+ MRU_OPVTHSSHR              100 (0x64)
+ IL_OPVT_RAWIMU_DATA        102 (0x66)
+ IL_OPVT_GNSSEXT_DATA       103 (0x67)
+ IL_USER_DEFINED_DATA       149 (0x95)
+```
+
+**Published Topics**
+
+Feel free to modify using fields from IL::INSDataStruct
+
+`/Inertial_Labs/sensor_data`  
+Gyro(x,y,z) , Accelation(x,y,z) , Magnetic (x,y,z) , Temprature , Input Voltage , Pressure , Barometric height.
+
+`/Inertial_Labs/ins_data`  
+GPS INS Time, GPS IMU Time, Millisecond of the week, Latitude, Longitude, Altitude, Heading , Pitch , Roll, Orientation quaternion, East Velocity, North Velocity, Up Velocity values, Solution status, Position STD, Heading STD, Unit Status.
+
+`/Inertial_Labs/gps_data`  
+Latitude, Longitude , Altitude , Ground Speed , Track Direction,  Vertical Speed values .
+
+`/Inertial_Labs/gnss_data`  
+GNSS service Info 1, Info 2, Satellites Used, Velocity Latency, Heading status, Heading, Pitch, GDOP, PDOP, HDOP, VDOP, TDOP, New GNSS Flag, Age of differenctiol correction, Position STD, Heading STD, Pitch STD.
+
+`/Inertial_Labs/marine_data`  
+Heave, Surge, Sway, Heave Velocity, Surge Velocity, Sway Velocity, Significant Wave Height.
+
+
+## FAQ
+1. **I use WSL2 with Linux and can't receive data from sensor.**\
+You need to use [usbipd](https://learn.microsoft.com/en-us/windows/wsl/connect-usb#install-the-usbipd-win-project). It allows to transfer data from USB to WSL2. And `/dev/ttyUSBx` devices will appear in Linux.
+Run PowerShell as Administrator and make something like:
+```powershell
+# Mount USB:
+usbipd list
+usbipd bind --busid 1-1
+usbipd attach --wsl --busid 1-1
+
+# Unmount USB:
+usbipd detach --busid 1-1
+```
+
+2. **The driver can't open my serial device?**\
+Make sure you have enough access to `/dev`.
+```bash
+sudo chmod 666 /dev/ttyUSB0
+```
+
+3. **Why I have permission error during the initialization process of the driver?**\
+Most often, this is because the baud rate you set does not match the package size to be received. Try increase the baud rate.
+```bash
+sudo stty -F /dev/ttyUSB0 460800
+ros2 run inertiallabs_ins il_ins --ros-args -p ins_url:=serial:/dev/ttyUSB0:460800 -p ins_output_format:=88
+```
+
+4. **How can I check data from sensor?**\
+Be sure, that Inertial Labs node has the topic-subscribers. Because messages will not send with no topic subscribers!
+
+Print topic example:
+In the separate windows run
+```bash
+source install/setup.bash
+ros2 topic list
+ros2 topic echo /Inertial_Labs/sensor_data
+```
+
+5. **Why is the IMU data output rate much lower than what is set?**\
+This may be due to a recent change in the FTDI USB-Serial driver in the Linux kernel, the following shell script might help:
+```bash
+# Reduce latency in the FTDI serial-USB kernel driver to 1ms
+# This is required due to https://github.com/torvalds/linux/commit/c6dce262
+for file in $(ls /sys/bus/usb-serial/devices/); do
+  value=`cat /sys/bus/usb-serial/devices/$file/latency_timer`
+  if [ $value -gt 1 ]; then
+    echo "Setting low_latency mode for $file"
+    sudo sh -c "echo 1 > /sys/bus/usb-serial/devices/$file/latency_timer"
+  fi
+done
+```
+
+6. **Why a field value is always zero?**\
+Most likely, because this field is not provided in the selected INS data packet. The most versatile data packet is User-Defined Data, which allows to order any set of fields
+
+## Bug Report
+Prefer to open an issue. You can also send an E-mail to support@inertiallabs.com.
---
+++ inertiallabs_ins/src/il_ins.cpp
@@ -0,0 +1,293 @@
+#include <iostream>
+#include <unistd.h>
+#include <math.h>
+#include <stdlib.h>
+
+#include <rclcpp/rclcpp.hpp>
+
+// Inertial Labs source header
+#include "ILDriver.h"
+
+// adding message type headers
+#include <inertiallabs_msgs/msg/sensor_data.hpp>
+#include <inertiallabs_msgs/msg/ins_data.hpp>
+#include <inertiallabs_msgs/msg/gps_data.hpp>
+#include <inertiallabs_msgs/msg/gnss_data.hpp>
+#include <inertiallabs_msgs/msg/marine_data.hpp>
+#include <inertiallabs_msgs/msg/imu_data.hpp>
+
+// Publishers
+
+struct Context
+{
+  rclcpp::Node::SharedPtr node;
+
+  rclcpp::Publisher<inertiallabs_msgs::msg::GnssData>::SharedPtr gnssDataPub;
+  rclcpp::Publisher<inertiallabs_msgs::msg::GpsData>::SharedPtr gpsDataPub;
+  rclcpp::Publisher<inertiallabs_msgs::msg::InsData>::SharedPtr insDataPub;
+  rclcpp::Publisher<inertiallabs_msgs::msg::MarineData>::SharedPtr marineDataPub;
+  rclcpp::Publisher<inertiallabs_msgs::msg::SensorData>::SharedPtr sensorDataPub;
+  rclcpp::Publisher<inertiallabs_msgs::msg::ImuData>::SharedPtr imuDataPub;
+
+  std::string imuFrameId;
+};
+
+void publishDevice(IL::INSDataStruct* data, void* contextPtr)
+{
+  Context* context = reinterpret_cast<Context*>(contextPtr);
+
+  const rclcpp::Time timestamp = context->node->now();
+  RCLCPP_INFO_STREAM(context->node->get_logger(),
+                     "Publishing data at timestamp " << timestamp.seconds() << "." << timestamp.nanoseconds());
+
+  if (context->sensorDataPub && context->sensorDataPub->get_subscription_count() > 0)
+  {
+    inertiallabs_msgs::msg::SensorData msg_sensor_data;
+    msg_sensor_data.header.stamp = timestamp;
+    msg_sensor_data.header.frame_id = context->imuFrameId;
+    msg_sensor_data.mag.x = data->Mag[0];
+    msg_sensor_data.mag.y = data->Mag[0];
+    msg_sensor_data.mag.z = data->Mag[0];
+    msg_sensor_data.accel.x = data->Acc[0];
+    msg_sensor_data.accel.y = data->Acc[1];
+    msg_sensor_data.accel.z = data->Acc[2];
+    msg_sensor_data.gyro.x = data->Gyro[0];
+    msg_sensor_data.gyro.y = data->Gyro[1];
+    msg_sensor_data.gyro.z = data->Gyro[2];
+    msg_sensor_data.temp = data->Temp;
+    msg_sensor_data.vinp = data->VSup;
+    msg_sensor_data.pressure = data->hBar;
+    msg_sensor_data.barometric_height = data->pBar;
+    context->sensorDataPub->publish(msg_sensor_data);
+  }
+
+  if (context->insDataPub && context->insDataPub->get_subscription_count() > 0)
+  {
+    inertiallabs_msgs::msg::InsData msg_ins_data;
+    msg_ins_data.header.stamp = timestamp;
+    msg_ins_data.header.frame_id = context->imuFrameId;
+    msg_ins_data.ypr.x = data->Heading;
+    msg_ins_data.ypr.y = data->Pitch;
+    msg_ins_data.ypr.z = data->Roll;
+    msg_ins_data.ori_quat.w = data->Quat[0];
+    msg_ins_data.ori_quat.x = data->Quat[1];
+    msg_ins_data.ori_quat.y = data->Quat[2];
+    msg_ins_data.ori_quat.z = data->Quat[3];
+    msg_ins_data.llh.x = data->Latitude;
+    msg_ins_data.llh.y = data->Longitude;
+    msg_ins_data.llh.z = data->Altitude;
+    msg_ins_data.vel_enu.x = data->VelENU[0];
+    msg_ins_data.vel_enu.y = data->VelENU[1];
+    msg_ins_data.vel_enu.z = data->VelENU[2];
+    msg_ins_data.gps_ins_time = data->GPS_INS_Time;
+    msg_ins_data.gps_imu_time = data->GPS_IMU_Time;
+    msg_ins_data.gps_msow.data = data->ms_gps;
+    msg_ins_data.solution_status.data = data->INSSolStatus;
+    msg_ins_data.usw = data->USW;
+    msg_ins_data.pos_std.x = data->KFLatStd;
+    msg_ins_data.pos_std.y = data->KFLonStd;
+    msg_ins_data.pos_std.z = data->KFAltStd;
+    msg_ins_data.heading_std = data->KFHdgStd;
+    context->insDataPub->publish(msg_ins_data);
+  }
+
+  if (context->gpsDataPub && context->gpsDataPub->get_subscription_count() > 0)
+  {
+    inertiallabs_msgs::msg::GpsData msg_gps_data;
+    msg_gps_data.header.stamp = timestamp;
+    msg_gps_data.header.frame_id = context->imuFrameId;
+    msg_gps_data.llh.x = data->LatGNSS;
+    msg_gps_data.llh.y = data->LonGNSS;
+    msg_gps_data.llh.z = data->AltGNSS;
+    msg_gps_data.hor_speed = data->V_Hor;
+    msg_gps_data.speed_dir = data->Trk_gnd;
+    msg_gps_data.ver_speed = data->V_ver;
+    context->gpsDataPub->publish(msg_gps_data);
+  }
+
+  if (context->gnssDataPub && context->gnssDataPub->get_subscription_count() > 0)
+  {
+    inertiallabs_msgs::msg::GnssData msg_gnss_data;
+    msg_gnss_data.header.stamp = timestamp;
+    msg_gnss_data.header.frame_id = context->imuFrameId;
+    msg_gnss_data.gnss_info_1 = data->GNSSInfo1;
+    msg_gnss_data.gnss_info_2 = data->GNSSInfo2;
+    msg_gnss_data.number_sat = data->SVsol;
+    msg_gnss_data.gnss_velocity_latency = data->GNSSVelLatency;
+    msg_gnss_data.gnss_angles_position_type = data->AnglesType;
+    msg_gnss_data.gnss_heading = data->Heading_GNSS;
+    msg_gnss_data.gnss_pitch = data->Pitch_GNSS;
+    msg_gnss_data.gnss_gdop = data->GDOP;
+    msg_gnss_data.gnss_pdop = data->PDOP;
+    msg_gnss_data.gnss_hdop = data->HDOP;
+    msg_gnss_data.gnss_vdop = data->VDOP;
+    msg_gnss_data.gnss_tdop = data->TDOP;
+    msg_gnss_data.new_gnss_flags = data->NewGPS;
+    msg_gnss_data.diff_age = data->DiffAge;
+    msg_gnss_data.pos_std.x = data->LatGNSSStd;
+    msg_gnss_data.pos_std.y = data->LonGNSSStd;
+    msg_gnss_data.pos_std.z = data->AltGNSSStd;
+    msg_gnss_data.heading_std = data->HeadingGNSSStd;
+    msg_gnss_data.pitch_std = data->PitchGNSSStd;
+    context->gnssDataPub->publish(msg_gnss_data);
+  }
+
+  if (context->marineDataPub && context->marineDataPub->get_subscription_count() > 0)
+  {
+    inertiallabs_msgs::msg::MarineData msg_marine_data;
+    msg_marine_data.header.stamp = timestamp;
+    msg_marine_data.header.frame_id = context->imuFrameId;
+    msg_marine_data.heave = data->Heave;
+    msg_marine_data.surge = data->Surge;
+    msg_marine_data.sway = data->Sway;
+    msg_marine_data.heave_velocity = data->Heave_velocity;
+    msg_marine_data.surge_velocity = data->Surge_velocity;
+    msg_marine_data.sway_velocity = data->Sway_velocity;
+    msg_marine_data.significant_wave_height = data->significant_wave_height;
+    context->marineDataPub->publish(msg_marine_data);
+  }
+  if (context->imuDataPub && context->imuDataPub->get_subscription_count() > 0)
+  {
+    inertiallabs_msgs::msg::ImuData msg_imu_data;
+    msg_imu_data.header.stamp = timestamp;
+    msg_imu_data.header.frame_id = context->imuFrameId;
+    msg_imu_data.ypr.x = data->Heading;
+    msg_imu_data.ypr.y = data->Pitch;
+    msg_imu_data.ypr.z = data->Roll;
+    msg_imu_data.accel.x = data->Acc[0];
+    msg_imu_data.accel.y = data->Acc[1];
+    msg_imu_data.accel.z = data->Acc[2];
+    msg_imu_data.gyro.x = data->Gyro[0];
+    msg_imu_data.gyro.y = data->Gyro[1];
+    msg_imu_data.gyro.z = data->Gyro[2];
+    context->imuDataPub->publish(msg_imu_data);
+  }
+}
+
+int main(int argc, char** argv)
+{
+  rclcpp::init(argc, argv);
+  auto options =
+      rclcpp::NodeOptions().allow_undeclared_parameters(true).automatically_declare_parameters_from_overrides(true);
+  rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("il_ins", options);
+  // rclcpp::Rate rate(100); // 100 hz
+
+  std::string port("serial:/dev/ttyUSB0:460800");
+  int insOutputFormat = 82;  // 0x52
+
+  Context context;
+  IL::Driver ins;
+
+  // Command line parameters
+  if (node->has_parameter("ins_url"))
+  {
+    port = node->get_parameter("ins_url").as_string();
+  }
+  if (node->has_parameter("ins_output_format"))
+  {
+    insOutputFormat = node->get_parameter("ins_output_format").as_int();
+  }
+
+  bool publish_sensor_data = false;
+  if (node->get_parameter_or("publish_sensor_data", publish_sensor_data, false))
+  {
+    if (publish_sensor_data)
+    {
+      context.sensorDataPub =
+          node->create_publisher<inertiallabs_msgs::msg::SensorData>("/Inertial_Labs/sensor_data", 1);
+    }
+  }
+
+  bool publish_ins_data = false;
+  if (node->get_parameter_or("publish_ins_data", publish_ins_data, false))
+  {
+    if (publish_sensor_data)
+    {
+      context.insDataPub = node->create_publisher<inertiallabs_msgs::msg::InsData>("/Inertial_Labs/ins_data", 1);
+    }
+  }
+
+  bool publish_gps_data = false;
+  if (node->get_parameter_or("publish_gps_data", publish_gps_data, false))
+  {
+    if (publish_gps_data)
+    {
+      context.gpsDataPub = node->create_publisher<inertiallabs_msgs::msg::GpsData>("/Inertial_Labs/gps_data", 1);
+    }
+  }
+
+  bool publish_gnss_data = false;
+  if (node->get_parameter_or("publish_gnss_data", publish_gnss_data, false))
+  {
+    if (publish_gnss_data)
+    {
+      context.gnssDataPub = node->create_publisher<inertiallabs_msgs::msg::GnssData>("/Inertial_Labs/gnss_data", 1);
+    }
+  }
+
+  bool publish_marine_data = false;
+  if (node->get_parameter_or("publish_marine_data", publish_marine_data, false))
+  {
+    if (publish_marine_data)
+    {
+      context.marineDataPub =
+          node->create_publisher<inertiallabs_msgs::msg::MarineData>("/Inertial_Labs/marine_data", 1);
+    }
+  }
+
+  bool publish_imu_data = false;
+  if (node->get_parameter_or("publish_imu_data", publish_imu_data, false))
+  {
+    if (publish_imu_data)
+    {
+      context.imuDataPub = node->create_publisher<inertiallabs_msgs::msg::ImuData>("/Inertial_Labs/imu_data", 1);
+    }
+  }
+
+  context.node = node;
+
+  // Communication with the device
+  RCLCPP_INFO(node->get_logger(), "Connecting to INS at URL %s\n", port.c_str());
+
+  int il_err = ins.connect(port.c_str());
+  if (il_err)
+  {
+    RCLCPP_FATAL(node->get_logger(), "Could not connect to the INS on this URL %s\n", port.c_str());
+    exit(EXIT_FAILURE);
+  }
+
+  if (ins.isStarted())
+  {
+    ins.stop();
+  }
+
+  const IL::INSDeviceInfo devInfo = ins.getDeviceInfo();
+  const IL::INSDevicePar devParams = ins.getDeviceParams();
+
+  const std::string serialNumber(reinterpret_cast<const char*>(devInfo.IDN), 8);
+  RCLCPP_INFO(node->get_logger(), "Found INS S/N %s\n", serialNumber.c_str());
+  context.imuFrameId = serialNumber;
+
+  il_err = ins.start(insOutputFormat);
+  if (il_err)
+  {
+    RCLCPP_FATAL(node->get_logger(), "Could not start the INS: %i\n", il_err);
+    ins.disconnect();
+    exit(EXIT_FAILURE);
+  }
+
+  ins.setCallback(&publishDevice, &context);
+
+  RCLCPP_INFO(node->get_logger(), "Publishing at %d Hz\n", devParams.dataRate);
+  RCLCPP_INFO(node->get_logger(),
+              "Run \"rostopic list\" to see all topics.\nRun \"rostopic echo <topic-name>\" to see the data from "
+              "sensor");
+
+  rclcpp::spin(node);
+  std::cout << "Stopping INS... " << std::flush;
+  ins.stop();
+  std::cout << "Disconnecting... " << std::flush;
+  ins.disconnect();
+  std::cout << "Done." << std::endl;
+  return 0;
+}
